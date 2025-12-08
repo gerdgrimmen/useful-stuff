c73e56894692e2877e5d4579372fbc7527063292
Gerd Grimmen <gerd.grimmen.gg@gmail.com>
2025-12-08 16:19:56 +0100

    add godot json processing link and snippet

--- godot/commands.sh
+++ godot/commands.sh
@@ -21,3 +21,22 @@ func load_from_file():
 	var file = FileAccess.open("user://save_game.dat", FileAccess.READ)
 	var content = file.get_as_text()
 	return content
+
+# JSON preocessing
+# https://docs.godotengine.org/en/stable/classes/class_json.html
+
+var data_to_send = ["a", "b", "c"]
+var json_string = JSON.stringify(data_to_send)
+# Save data
+# ...
+# Retrieve data
+var json = JSON.new()
+var error = json.parse(json_string)
+if error == OK:
+	var data_received = json.data
+	if typeof(data_received) == TYPE_ARRAY:
+		print(data_received) # Prints the array.
+	else:
+		print("Unexpected data")
+else:
+	print("JSON Parse Error: ", json.get_error_message(), " in ", json_string, " at line ", json.get_error_line())
