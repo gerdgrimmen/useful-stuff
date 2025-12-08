953324fcb170e4809bad8ab543fca2ffd710c7cc
Gerd Grimmen <gerd.grimmen.gg@gmail.com>
2025-12-08 16:19:11 +0100

    add godot file access snippet and link

--- godot/commands.sh
+++ godot/commands.sh
@@ -8,3 +8,16 @@ Default Texture Filter -> "Nearest"
 [rendering]
 
 textures/canvas_textures/default_texture_filter=0
+
+
+# File Access
+# https://docs.godotengine.org/en/stable/classes/class_fileaccess.html
+
+func save_to_file(content):
+	var file = FileAccess.open("user://save_game.dat", FileAccess.WRITE)
+	file.store_string(content)
+
+func load_from_file():
+	var file = FileAccess.open("user://save_game.dat", FileAccess.READ)
+	var content = file.get_as_text()
+	return content
