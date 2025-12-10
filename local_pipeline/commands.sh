b67108a6d9aee6cb91a091054c3817f3bce8599e
Gerd Grimmen <gerd.grimmen.gg@gmail.com>
2025-12-10 01:58:04 +0100

    add go-task commands

--- local_pipeline/commands.sh
+++ local_pipeline/commands.sh
@@ -1,2 +1,13 @@
 # links
 # https://taskfile.dev/
+
+# install
+# i use arch btw
+pacman -S go-task
+
+
+# initialize a taskfile
+go-task --init
+
+# run default part of local pipeline
+go-task default
